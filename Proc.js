8243b92c6e1fd78e682cf739bd938bf898f97278
divyatarange <divyatarange99@gmail.com>
2024-04-02 18:43:22 +0530

    My commit

---
+++ Proc.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "150px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+}
